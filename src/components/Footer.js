1e44d9bc139e612f9e6402beda1e2f64a86341d7
unknown <mukherjeesrinjan@gmail.com>
2024-09-11 22:08:41 +0530

    Edited Footer again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <Layout className="py-12 flex items-center justify-between lg:flex-col lg:py-6">
             <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
             <div className="flex items-center lg:py-2">
-                Build With <span className="text-primary text-2xl px-1">&#9825;</span>by&nbsp;<Link href="/" className="underline underline-offset-2">Srinjan Mukherjee</Link>
+                Built by <span className="text-primary text-2xl px-1">&#9825;</span>by&nbsp;<Link href="/" className="underline underline-offset-2">Srinjan Mukherjee</Link>
             </div>
             <Link href="/" target={"_blank"} className="underline underline-offset-2">Say Hello &#128075;</Link>
         </Layout>
