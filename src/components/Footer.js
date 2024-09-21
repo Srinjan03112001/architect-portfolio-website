39ea685c6e812cb0212393245e70af53276addbb
unknown <mukherjeesrinjan@gmail.com>
2024-09-21 22:27:45 +0530

    made the footer border much better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from './Layout'
 import Link from 'next/link'
+import useThemeSwitcher from '../components/hooks/useThemeSwitcher'
 
 const Footer = () => {
+
+  const [mode] = useThemeSwitcher(); // Get the current theme (light or dark)
+
   return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base ">
+    <footer className={`w-full font-medium text-lg sm:text-base  dark:bg-gradient-to-t dark:from-black dark:via-black/70 dark:to-transparent bg-gradient-to-t from-white via-white/50 to-transparent dark:text-light text-dark`}>
         <Layout className="py-12 flex items-center justify-between lg:flex-col lg:py-6">
             <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
             <div className="flex items-center lg:py-2">
